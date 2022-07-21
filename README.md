03df609611c8aaac9f5a8e6fe38ccf3b75e09c3e
Shaun Clarke <shaunclarke43@gmail.com>
2022-07-21 10:49:43 -0700

    updated folder structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # terraform-deploy-multiple-environments
 Using terraform modules to deploy to multiple environments
+Modules are defined in a base directory
+This allows you to deploy the same config in dev, test and prod environments
--- dev/variables.tf
+++ dev/variables.tf
@@ -1,4 +1,4 @@
-# public subnet ip addresses CIDR blocks
+# public subnet ip addresses CIDR blocks for dev
 variable "public_subnet_cidr" {
   description = "public subnet ip addresses CIDR blocks"
   default = [
@@ -6,11 +6,12 @@ variable "public_subnet_cidr" {
   ]
 }
 
-# variable for environment tagging
+# variable for environment tagging in dev
 variable "env" {
   default = "dev"
 }
 
+# vpc cidr block for the dev environment
 variable "vpc_cidr" {
   default = "10.3.0.0/16"
 }
